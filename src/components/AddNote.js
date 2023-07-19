b829c88def6c6673a73757d542e3f0e401027647
Krishantchauhan <krashantchauhan@gmail.com>
2023-07-19 15:32:19 +0530

    set new note text added in AddNote
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,14 +1,30 @@
+import { useState } from "react";
+
 const AddNote = () => {
+  const [noteText, setNoteText] = useState("");
+
+  const handleChange = (event) => {
+    setNoteText(event.target.value);
+  };
+  
+  const handleSaveClick = () => {
+    
+  };
+
+
   return (
     <div className="note new">
       <textarea
         rows="8"
         cols="10"
-        placeholder="Type to add a note...."></textarea>
-
+        placeholder="Type to add a note...."
+        value={noteText}
+        onChange={handleChange}></textarea>
       <div className="note-footer">
         <small>200 Remaing</small>
-        <button className="save">Save</button>
+        <button className="save" onClick={handleSaveClick}>
+          Save
+        </button>
       </div>
     </div>
   );
