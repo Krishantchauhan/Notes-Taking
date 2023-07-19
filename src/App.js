61ccf3c78e81d0f42bdd67c141bd29dcbd810452
Krishantchauhan <krashantchauhan@gmail.com>
2023-07-19 14:40:25 +0530

    array for notes created
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
-import NotesList from './components/NotesList';
-
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import NotesList from "./components/NotesList";
 
 const App = () => {
-  return <div className='container'>
-    <NotesList/>
-  </div>;
-};
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first Note!",
+      date: "19/07/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my Second Note!",
+      date: "20/07/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my Third Note!",
+      date: "21/07/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my Fourth Note!",
+      date: "22/07/2021",
+    },
+    {
+      id: nanoid(),
+      text: "This is my Fifth Note!",
+      date: "23/07/2021",
+    },
+  ]);
 
+  return (
+    <div className="container">
+      <NotesList />
+    </div>
+  );
+};
 
 export default App;
