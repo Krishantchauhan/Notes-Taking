071846df78307550525ed769f046169fe9b1e9ec
Krishantchauhan <krashantchauhan@gmail.com>
2023-07-19 17:30:31 +0530

    Search logic :)
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- src/App.js
+++ src/App.js
@@ -32,6 +32,8 @@ const App = () => {
     },
   ]);
 
+  const [searchText, setSearchText] = useState("");
+
   const addNote = (text) => {
     const date = new Date();
     const newNote = {
@@ -50,9 +52,11 @@ const App = () => {
 
   return (
     <div className="container">
-      <Search />
+      <Search handleSearchNote={setSearchText} />
       <NotesList
-        notes={notes}
+        notes={notes.filter((note) =>
+          note.text.toLowerCase().includes(searchText)
+        )}
         handleAddNote={addNote}
         handleDeleteNote={deleteNote}
       />
