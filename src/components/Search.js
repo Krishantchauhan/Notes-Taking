8f279da0ff8bbfdaeb000c899104c317bf25e7c7
Krishantchauhan <krashantchauhan@gmail.com>
2023-07-20 12:42:49 +0530

    css improved background and search bar ans many more
    Signed-off-by: Krishantchauhan <krashantchauhan@gmail.com>

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,25 @@
-import React from "react";
-import { MdSearch } from "react-icons/md";
+import React from 'react';
+import { MdSearch } from 'react-icons/md';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 
 const search = ({ handleSearchNote }) => {
   return (
-    <div className="search">
-      <MdSearch className="search-icons" size="1.3em" />
+    <div className="search-container">
       <input
-        onChange={(event) => handleSearchNote(event.target.value)}
         type="text"
-        placeholder="Type to Search...."
+        name="search"
+        placeholder="Search..."
+        className="search-input"
+        onChange={(event) => handleSearchNote(event.target.value)}
       />
+      <a href="#" className="search-btn">
+        <FontAwesomeIcon icon={faSearch} />
+      </a>
     </div>
   );
 };
 
 export default search;
+ 
